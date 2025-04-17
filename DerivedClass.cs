55f8537edb5d5dbfb70956482ded63835e045681
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-04-17 10:29:10 +0300

    Add files via upload

---
+++ DerivedClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37_3
+{
+    internal class DerivedClass : IInterface3
+    {
+        public void Method1()
+        {
+            Console.Write("Метод Method1");
+        }
+        public void Method2()
+        {
+            Console.Write("Метод Method2");
+        }
+        public void Method3()
+        {
+            Console.Write("Метод Method3");
+        }
+    }
+}
---
+++ IInterface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37_3
+{
+    internal interface IInterface1
+    {
+        void Method1();
+    }
+}
