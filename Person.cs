e41308633b60670b1d4f634f493fbe13b1f8564d
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-04-17 10:34:12 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37_4
+{
+    internal class Person : ISpeak1, ISpeak2
+    {
+        public void Say1()
+        {
+            Console.WriteLine("Говорит громко - Я люблю программирование!");
+        }
+
+        public void Think()
+        {
+            Console.WriteLine("Думает - А C# больше всего");
+        }
+
+        public void Say3()
+        {
+            Console.WriteLine("Говорит нормально - Меньше люблю компьютерные игры");
+        }
+    }
+}
