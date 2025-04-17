1d23c1cec9804cc1e3539ad8d993a7d1c9aaf0d3
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-04-17 10:17:56 +0300

    Add files via upload

---
+++ DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class DerivedClass : IInterface
+    {
+        public void Method()
+        {
+            Console.Write("Привет!");
+        }
+    }
+}
